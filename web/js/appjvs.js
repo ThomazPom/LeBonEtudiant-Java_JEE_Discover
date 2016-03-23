f11f55addef7b92e12ba97ee01287d868dcb0620
Thomas Benhamou <Thomas Benhamou>
2016-03-23 15:04:55 +0100

    Création du template

--- web/js/appjvs.js
+++ web/js/appjvs.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+ var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 48.8534100, lng: 2.3488000},
+          zoom: 6
+        });
+        
+      }
+$(document).ready(function (){
+    //Code à exécuter apres le chargement de la page
+    initMap();
+})
