838e8745886680527700110b7e255f8786f56293
ThomazPom <Thomas.benhamou@hotmail.com>
2016-03-26 13:49:29 +0100

    Création des entitées

---
+++ src/model/User.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Thomas
+ */
+@Entity
+public class User {
+    
+}
