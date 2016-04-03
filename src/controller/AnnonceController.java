f1b2af2b28107ead5d0c9ff4108f681081eba789
myriamrouis <myriamrouis@gamil.com>
2016-04-03 16:17:54 +0200

    Edition annonce

--- src/controller/AnnonceController.java
+++ src/controller/AnnonceController.java
@@ -18,6 +18,7 @@ import model.Annonce;
 import model.Categorie;
 import model.Etablissement;
 import model.Utilisateur;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 /**
  *
@@ -40,35 +41,7 @@ public class AnnonceController {
         em.persist(a);
         return a;
     }
-
-    public Annonce majAnnonce(int id,String titre, int prix, String numeroOverride, String emailOverride, String Description, boolean active, List<Categorie> categories, List<Etablissement> etablissements) {
-        Annonce annonce =  getAnnonceById(id);
-        if(annonce!=null)
-        {
-            if(prix!=-1) annonce.setPrix(prix);
-            if(numeroOverride!=null) annonce.setNumeroOverride(numeroOverride);
-            if(emailOverride!=null) annonce.setEmailOverride(emailOverride);
-            if(Description!=null) annonce.setPrix(prix);
-            em.persist(annonce);
-            return annonce;
-        }
-        return null;
-    }
-
-    public Annonce getAnnonceById(int id) {
-        System.out.println("-->>getAnnonceById()");
-        Query q = em.createQuery("SELECT a from Annonce c where a.id=:id");
-        q.setParameter("id", id);
-        List<Annonce> listAnnonce = q.getResultList();
-
-        Iterator<Annonce> i = listAnnonce.iterator();
-        if (i.hasNext()) {
-            return i.next();
-        }
-        System.out.println("Annonce : " + id +" is not an ID");
-        return null;
-    }
-
+    
     public Annonce creerAnnonce(Utilisateur Proprietaire, String Titre, String prix, String numeroOverride, String emailOverride, String Description, boolean active, String[] categories, String[] etablissements) {
         ArrayList<Categorie> arcateg = new ArrayList<>();
         ArrayList<Etablissement> aretab = new ArrayList<>();
@@ -109,5 +82,83 @@ public class AnnonceController {
         }
 
         return creerAnnonce(Proprietaire, Titre, prixCreate, numeroOverride, emailOverride, Description, active, arcateg, aretab);
+    }
+    
+    public Annonce majAnnonce(int id, String titre, int prix, String numeroOverride, String emailOverride, String Description, boolean active, List<Categorie> categories, List<Etablissement> etablissements) {
+        Annonce annonce =  getAnnonceById(id);
+        if(annonce!=null)
+        {
+            if(prix!=-1) annonce.setPrix(prix);                                 
+            if(numeroOverride!=null) annonce.setNumeroOverride(numeroOverride);
+            if(emailOverride!=null) annonce.setEmailOverride(emailOverride);
+            if(Description!=null) annonce.setDescription(Description);
+            if(!active) annonce.setActive(active);                              //si l'annonce est desactivee on l'active.
+            if(!categories.isEmpty()) annonce.setCategories(categories);
+            if(!etablissements.isEmpty()) annonce.setEtablissements(etablissements);
+            
+            em.persist(annonce);
+            return annonce;
+        }
+        return null;
+    }
+    
+    public Annonce majAnnonce(String id, String titre, String prix, String numeroOverride, String emailOverride, String Description, boolean active, String[] categories, String[] etablissements){
+        ArrayList<Categorie> arcateg = new ArrayList<>();
+        ArrayList<Etablissement> aretab = new ArrayList<>();
+        
+        int idAnnonce = -1;
+        try {
+            idAnnonce = Integer.parseInt(id);
+        } catch (Exception e) {
+            System.err.println(id + "is not an id (majAnnonce)");
+        }
+        for (String idA : categories) {
+            int idCateg = -1;
+            try {
+                idCateg = Integer.parseInt(idA);
+            } catch (Exception e) {
+                System.err.println(idA + " is not an id (majAnnonce)");
+                return null;
+            }
+            Categorie et = cc.getCategoriesById(idCateg);
+            if (et != null) {
+                arcateg.add(et);
+            }
+        }
+        for (String idA : etablissements) {
+            int idEtab = -1;
+            try {
+                idEtab = Integer.parseInt(idA);
+            } catch (Exception e) {
+                System.err.println(idA + " is not an id (majAnnonce)");
+                return null;
+            }
+            Etablissement et = ec.getEtablissementByID(idEtab);
+            if (et != null) {
+                aretab.add(et);
+            }
+        }
+        
+        int prixAnnonce = 999999;
+        try {
+            prixAnnonce = Integer.parseInt(prix);
+        } catch (Exception e) {
+            System.err.println(prix + "is not a number (majAnnonce)");
+        }
+        return majAnnonce(idAnnonce, titre, prixAnnonce, numeroOverride, emailOverride, Description, active, arcateg, aretab);
+    }
+
+    public Annonce getAnnonceById(int id) {
+        System.out.println("-->>getAnnonceById()");
+        Query q = em.createQuery("SELECT a from Annonce c where a.id=:id");
+        q.setParameter("id", id);
+        List<Annonce> listAnnonce = q.getResultList();
+
+        Iterator<Annonce> i = listAnnonce.iterator();
+        if (i.hasNext()) {
+            return i.next();
+        }
+        System.out.println("Annonce : " + id +" is not an ID");
+        return null;
     }
 }
